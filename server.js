f5f51c12d40647e7167addad6e7f578a7e9124ad
kharoteswati <kharoteswati31@gmail.com>
2023-07-06 10:42:52 +0530

    tp

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults();
 const cors = require("cors")
 
-application.use(cors)
+server.use(cors())
 server.use(middlewares)
 server.use('', router)
 server.listen(process.env.PORT || 5000, () => {
